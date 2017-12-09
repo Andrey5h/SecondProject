e558bd0d4d0c0d145405ecedeec619aa1b5f7e9c
Andrey5h <maxhaste@yandex.ru>
2017-12-09 21:52:43 +0300

    UpdateCommit

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,5 @@
 /**
- *@author Andrey Strelnikov
+ * @author Andrey Strelnikov
  * My second programm
  */
 
@@ -17,23 +17,34 @@ public class Base {
             System.out.println(+i + " элимент массива = " + array[i]);
         }
         // Поиск Максимально отрицательного элимента
+        int k = 0;
         int min = 0;
         int max = -10;
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min && array[i] > max) {
                 max = array[i];
+                k = i;
             }
         }
-        System.out.println("Максимально отрицательный элемент = " + max);
+        System.out.println("Максимально отрицательный элемент = " + max + " Его индекс = " + k);
 
         // Поиск минимально положительного элимента
+        int b = 0;
         int min1 = 0;
         int max1 = 10;
         for (int a = 0; a < array.length; a++) {
             if (array[a] > min1 && array[a] < max1) {
                 max1 = array[a];
+                b=a;
             }
         }
-        System.out.println("Минимально положительный элемент = " + max1);
+        System.out.println("Минимально положительный элемент = " + max1 + " Его индекс = "+b);
+        //Меняем местами элименты
+        array[k]= max1;
+        array[b]= max;
+        System.out.println("Индекс = "+k +" Элимент = " +array[k]);
+        System.out.println("Индекс = "+b +" Элимент = " +array[b]);
+
+        }
     }
 }
